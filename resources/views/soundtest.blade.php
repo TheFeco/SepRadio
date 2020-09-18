1aa64715df8a05fc33384bf8e0954e0e62ae97b7
unknown <francisco.fcospinoza@gmail.com>
2020-09-17 18:00:08 -0600

    first commit

---
+++ resources/views/soundtest.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todamiescuela Radio</title>
+    <link rel="stylesheet" href="{{asset('css/all.css')}}">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-12 col xs-12">
+            hey
+            </div>
+        </div>
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="{{asset('js/all.js')}}"></script>
+</html>
